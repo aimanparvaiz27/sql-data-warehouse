3a7803c488d71f304b8c06ce52a9f9c6344554c4
Aiman Parvaiz <113871236+aimanparvaiz27@users.noreply.github.com>
2025-10-14 14:34:37 +0500

    Update 00_init_database.sql

--- Scripts/00_init_database.sql
+++ Scripts/00_init_database.sql
@@ -3,11 +3,35 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'DataWarehouseAnalytics' after checking if it already exists. 
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additionally, this script creates a schema called gold
 	
 WARNING:
-    Running this script will drop the entire 'DataWarehouseAnalytics' database if it exists. 
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
+
+USE master;
+
+-- Drop and recreate the 'DataWarehouseAnalytics' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+
+CREATE SCHEMA gold;
+GO
+
+
